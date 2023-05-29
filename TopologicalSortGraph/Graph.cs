8f837882e880e13114545116cea08ef9ce00b38c
Чинчи Саны-Шири <Sany-ShiriCE@mer.ci.nsu.ru>
2023-05-29 11:06:10 +0700

    Добавьте файлы проекта.

---
+++ TopologicalSortGraph/Graph.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopologicalSortGraph
+{
+    public class Graph
+    {
+        private int verticesCount;
+        private List<int>[] adjacencyList;
+
+        public Graph(int v)
+        {
+            verticesCount = v;
+            adjacencyList = new List<int>[verticesCount];
+            for (int i = 0; i < verticesCount; i++)
+            {
+                adjacencyList[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int source, int destination)
+        {
+            adjacencyList[source].Add(destination);
+        }
+
+        public List<int> TopologicalSort()
+        {
+            List<int> result = new List<int>();
+            bool[] visited = new bool[verticesCount];
+
+            for (int i = 0; i < verticesCount; i++)
+            {
+                if (!visited[i])
+                {
+                    DFS(i, visited, result);
+                }
+            }
+
+            result.Reverse();
+            return result;
+        }
+
+        private void DFS(int vertex, bool[] visited, List<int> result)
+        {
+            visited[vertex] = true;
+
+            foreach (int neighbor in adjacencyList[vertex])
+            {
+                if (!visited[neighbor])
+                {
+                    DFS(neighbor, visited, result);
+                }
+            }
+
+            result.Add(vertex);
+        }
+    }
+}
---
+++ TopologicalSortGraph/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TopologicalSortGraph
+{
+    public class Program // Графы, топологическая сортировка
+    {
+        public static void Main()
+        {
+            int verticesCount = 9;
+            Graph graph = new Graph(verticesCount);
+
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(2, 5);
+            graph.AddEdge(4, 8);
+            graph.AddEdge(5, 8);
+            graph.AddEdge(3, 6);
+            graph.AddEdge(6, 7);
+            graph.AddEdge(7, 8);
+
+            List<int> sortedVertices = graph.TopologicalSort();
+
+            Console.WriteLine("Результат топологической сортировки:");
+            foreach (int vertex in sortedVertices)
+            {
+                Console.Write(vertex + " ");
+            }
+        }
+    }
+}
